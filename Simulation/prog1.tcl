dbee31069c9627c4fc2a9bdfd14d021afea93c66
Adityakr49 <adityakumarpilot@gmail.com>
2023-12-28 09:15:01 +0530

    G

---
+++ Simulation/prog1.tcl
@@ -0,0 +1,55 @@
+#Create Simulator
+set ns [new Simulator]
+
+set ntrace [open prog1.tr w]
+$ns trace-all $ntrace
+set namfile [open prog1.nam w]
+$ns namtrace-all $namfile
+
+proc Finish {} {
+    global ns ntrace namfile
+
+    $ns flush-trace
+    close $ntrace
+    close $namfile
+
+    exec nam prog1.nam &
+
+    exec echo "The number of packet drops is " &
+    exec grep -c "^d" prog1.tr &
+    exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+$n0 label "TCP Source"
+$n2 label "Sink"
+
+$ns color 1 blue
+
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+
+$ns queue-limit $n0 $n1 10
+$ns queue-limit $n1 $n2 10
+
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+set sink0 [new Agent/TCPSink]
+$ns attach-agent $n2 $sink0
+
+$ns connect $tcp0 $sink0
+
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 500
+$cbr0 set rate_ 1Mb
+$cbr0 attach-agent $tcp0
+
+$cbr0 attach-agent $tcp0
+
+$ns at 0.0 "$cbr0 start"
+$ns at 5.0 "Finish"
+
+$ns run
